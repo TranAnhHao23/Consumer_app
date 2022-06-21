b7902a19b5cf15eadc07bcc14ce208e2e97023e4
pxcanh-cmc <pxcanh.cmc@gmail.com>
2022-06-21 17:03:46 +0700

    create API for creating location

--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -2,33 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { LocationsService } from './locations.service';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('locations')
 @Controller('locations')
 export class LocationsController {
   constructor(private readonly locationsService: LocationsService) {}
 
   @Post()
-  create(@Body() createLocationDto: CreateLocationDto) {
-    return this.locationsService.create(createLocationDto);
+  async create(@Body() createLocationDto: CreateLocationDto) {
+    const savedLocation = await this.locationsService.create(createLocationDto);
+    return {
+      data: savedLocation
+    }
   }
 
-  @Get()
-  findAll() {
-    return this.locationsService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.locationsService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.locationsService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.locationsService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLocationDto: UpdateLocationDto) {
-    return this.locationsService.update(+id, updateLocationDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateLocationDto: UpdateLocationDto) {
+  //   return this.locationsService.update(+id, updateLocationDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.locationsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.locationsService.remove(+id);
+  // }
 }
