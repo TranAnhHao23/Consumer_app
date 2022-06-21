2907eb073b5efdcca85194e0237277f820fb29d7
pxcanh-cmc <pxcanh.cmc@gmail.com>
2022-06-21 17:02:46 +0700

    create dto for creating location

--- src/modules/locations/dto/create-location.dto.ts
+++ src/modules/locations/dto/create-location.dto.ts
@@ -1,1 +1,44 @@
-export class CreateLocationDto {}
+
+import { ApiProperty } from '@nestjs/swagger'
+import {IsUUID, IsNotEmpty, IsDecimal, IsString, Length, IsNumber, IsInt, Min, Max, IsOptional} from 'class-validator'
+
+export class CreateLocationDto {
+    @ApiProperty()
+    @IsNumber()
+    longitude: number
+
+    @ApiProperty()
+    @IsNumber()
+    latitude: number
+
+    @ApiProperty({
+        maxLength: 255
+    })
+    @IsNotEmpty()
+    @IsString()
+    @Length(0, 255)
+    address: string
+
+    @ApiProperty({
+        maxLength: 255,
+        required: false
+    })
+    @IsString()
+    @IsOptional()
+    @Length(0, 255)
+    note: string
+
+    @ApiProperty()
+    @IsUUID()
+    tripId: string
+
+    @ApiProperty({
+        description: "0 – source, 1 – first destination, 2 – second destination, 3 – third destination",
+        minimum: 0,
+        maximum: 3
+    })
+    @IsInt()
+    @Min(0)
+    @Max(3)
+    milestone: number
+}
