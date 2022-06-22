c28fc13e61a2579261556cae2f50281e37cbe0fa
Mong Nguyen <nhmong@cmcglobal.vn>
2022-06-22 12:54:04 +0700

    update Swagger model

--- src/modules/favourite_locations/dto/create-favourite_location.dto.ts
+++ src/modules/favourite_locations/dto/create-favourite_location.dto.ts
@@ -1,21 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsString } from 'class-validator';
 
-export class CreateFavouriteLocationDto {
+export class CreateFavouriteLocationDto{
+  @ApiProperty()
   @IsString()
   userId: string;
 
+  @ApiProperty()
   @IsString()
   title: string;
 
+  @ApiProperty()
   @IsString()
   longitude: string;
 
+  @ApiProperty()
   @IsString()
   latitude: string;
 
+  @ApiProperty()
   @IsString()
   address: string;
 
+  @ApiProperty()
   @IsString()
   note: string;
 }
