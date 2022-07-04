c2600152eeaaa36a2e2cd11186b278048ebce081
Mong Nguyen <nhmong@cmcglobal.vn>
2022-07-04 14:04:02 +0700

    remove redis cache

--- src/modules/user_session/dto/update-user_session.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserSessionDto } from './create-user_session.dto';
-
-export class UpdateUserSessionDto extends PartialType(CreateUserSessionDto) {
-  user_id: string;
-  token: string;
-}
