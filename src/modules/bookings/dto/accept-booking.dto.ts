a1b09e9b3a62e32c5db3e423c34c345b4fcd0322
pxcanh-cmc <pxcanh.cmc@gmail.com>
2022-07-06 11:26:25 +0700

    mock data accept booking

--- src/modules/bookings/dto/accept-booking.dto.ts
+++ src/modules/bookings/dto/accept-booking.dto.ts
@@ -1,115 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsOptional, IsString } from "class-validator";
+import { IsNumber, IsObject, IsOptional, IsString } from "class-validator";
+import { CarInfoDto } from "./car-info.dto";
+import { DriverInfoDto } from "./driver-info.dto";
 export class AcceptBookingDto {
-    @ApiProperty({
-        required: false
-        })
-    @IsOptional()
-    @IsString()
-    carId: string;
+    // @ApiProperty()
+    // @IsString()
+    // driverBookingId: string
 
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    carTypeId: string
+    @ApiProperty()
+    @IsObject()
+    carInfo: CarInfoDto;
 
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    carIcon: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    carSize: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    carLicensePlateNumber: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    carBranch: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    carColor: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    carRegion: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    driverId: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    driverName: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    driverAvatar: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsString()
-    driverPhoneNum: string
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsNumber()
-    driverRating: number
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsNumber()
-    driverLatitude: number
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsNumber()
-    driverLongitude: number
-
-    @ApiProperty({
-        required: false
-    })
-    @IsOptional()
-    @IsNumber()
-    driverStatus: number
+    @ApiProperty()
+    @IsObject()
+    driverInfo: DriverInfoDto
 }
